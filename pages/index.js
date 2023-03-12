cc6bf7485a71a1ba853b24aa9fe5af56fe1b8557
zafaremon20 <zafar emon>
2023-03-12 19:31:38 +0600

    conditionally add todos

--- pages/index.js
+++ pages/index.js
@@ -2,54 +2,82 @@ import Link from "next/link";
 import { useEffect } from "react";
 import { useState } from "react";
 
+// const getTds = () => {
+//   if (typeof window !== 'undefined') {
+//     return JSON.parse(window.localStorage.getItem('todos'));
+//   }
+// }
+
 const HomePage = () => {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    const todoLists = localStorage.getItem('todos')
+    const todoLists = localStorage.getItem("todos");
     if (todoLists) {
-      setTodos(JSON.parse(todoLists))
+      setTodos(JSON.parse(todoLists));
     }
   }, []);
 
   const addTodo = (e) => {
     e.preventDefault();
     const todoText = e.target.todo.value;
-    const allTodo = [{ todoText: todoText, id: Date.now(), completed: false }, ...todos]
+    const allTodo = [
+      { todoText: todoText, id: Date.now(), completed: false },
+      ...todos,
+    ];
     if (!todoText) {
-
     } else {
-      setTodos(allTodo)
-      localStorage.setItem('todos', JSON.stringify(allTodo))
-      e.target.todo.value = '';
+      // if todos length is 5 and some todos are completed
+      // if todos length is < 5
+      const incomplete = todos.filter((todo) => todo.completed !== true);
+      const complete = todos.filter((todo) => todo.completed === true);
+      if (todos.length < 5) {
+        setTodos(allTodo);
+        localStorage.setItem("todos", JSON.stringify(allTodo));
+        e.target.todo.value = "";
+      } else if (incomplete.length < 5 && complete) {
+        setTodos(allTodo);
+        localStorage.setItem("todos", JSON.stringify(allTodo));
+        let confirm = window.confirm(
+          "Do you want to cleared Completeed todos?"
+        );
+        if (confirm) {
+          clearCompleted();
+        }
+      } else {
+        e.target.todo.value = "";
+        alert("Complete previous todo");
+      }
+      // setTodos(allTodo);
+      // localStorage.setItem("todos", JSON.stringify(allTodo));
     }
-  }
+  };
 
   const handleComplete = (id) => {
-    let newTodoList = todos.filter(todo => {
+    let newTodoList = todos.filter((todo) => {
       if (todo.id === id) {
         todo.completed = !todo.completed;
       }
-      return todo
-    })
-    setTodos(newTodoList)
-    localStorage.setItem('todos', JSON.stringify(newTodoList))
-  }
+      return todo;
+    });
+    setTodos(newTodoList);
+    localStorage.setItem("todos", JSON.stringify(newTodoList));
+  };
 
   const deleteTodo = (id) => {
     const remainTodos = todos.filter((todo) => todo.id !== id);
 
     setTodos(remainTodos);
-    localStorage.setItem('todos', JSON.stringify(remainTodos))
-  }
+    localStorage.setItem("todos", JSON.stringify(remainTodos));
+  };
 
   const clearCompleted = () => {
-    let newTodoList = todos.filter(todo => {
-      if (!todo.completed) return todo
-    })
+    let newTodoList = todos.filter((todo) => {
+      if (!todo.completed) return todo;
+    });
     setTodos(newTodoList);
-    localStorage.setItem('todos', JSON.stringify(newTodoList))
-  }
+    localStorage.setItem("todos", JSON.stringify(newTodoList));
+  };
 
   return (
     <div className="main-div">
@@ -57,22 +85,49 @@ const HomePage = () => {
         <h2>Lets Make Todo</h2>
         <div className="todo-div">
           <form className="todo-input" onSubmit={addTodo}>
-            <input type="text" name="todo" id="todo" placeholder="Your Todo" required />
-            <button type="submit" title="Add Todo">+</button>
+            <input
+              type="text"
+              name="todo"
+              id="todo"
+              placeholder="Your Todo"
+              required
+            />
+            <button type="submit" title="Add Todo">
+              +
+            </button>
           </form>
           <div className="all-todo">
-            {
-              todos?.slice(0, 5).map(({ id, completed, todoText }) => <div key={id} style={{ background: completed && "green" }} className="todo">
-                <input onChange={() => handleComplete(id)} type="checkbox" name="completed" id="completed" checked={completed} />
-                <Link href={`/${id}`}><p>{todoText}</p></Link>
-                <button title="Delete Todo" onClick={() => deleteTodo(id)}>&#9003;</button>
-              </div>)
-            }
-            <button onClick={clearCompleted} className="clear-btn">Clear Completed Todo</button>
+            {todos?.map(({ id, completed, todoText }) => (
+              <div
+                key={id}
+                style={{ background: completed && "green" }}
+                className="todo"
+              >
+                <input
+                  onChange={() => handleComplete(id)}
+                  type="checkbox"
+                  name="completed"
+                  id="completed"
+                  checked={completed}
+                />
+                <Link href={`/${id}`}>
+                  <p>
+                    {todoText.split(" ").slice(0, 5).join(" ")}
+                    {todoText.split(" ").length > 5 && "..."}
+                  </p>
+                </Link>
+                <button title="Delete Todo" onClick={() => deleteTodo(id)}>
+                  &#9003;
+                </button>
+              </div>
+            ))}
+            <button onClick={clearCompleted} className="clear-btn">
+              Clear Completed Todo
+            </button>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 export default HomePage;
